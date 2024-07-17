812a126acbc8de20a9133481dae8ace8b679f55b
shuvajitgayen <shuvajitgayen57@gmail.com>
2024-07-17 10:27:13 +0530

    add api secure and edit file path

--- src/index.js
+++ src/index.js
@@ -8,27 +8,52 @@ app.use(express())
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+
+const casheOptions = {
+  Headers:{
+    'Cache-Control':'../public,max-age=86400'
+  }
+}
+
 app.get("/",(req,res)=>{
   res.sendFile(path.join(__dirname,'../public/index.html'))
 })
+
 app.get("/style.css",(req,res)=>{
-  res.sendFile(path.join(__dirname,"../public/style.css"))
+  const referer = req.get('Referer')
+  if(referer && referer.startsWith(`http://localhost:${port}`)){
+    res.sendFile(path.join(__dirname,"./style.css"),casheOptions)
+  }else{
+    res.status(403).send('Forbidden')
+  }
 })
+
 app.get("/script.js",(req,res)=>{
-  res.sendFile(path.join(__dirname,"../public/script.js"))
+  const referer = req.get('Referer')
+  if(referer && referer.startsWith(`http://localhost:${port}`)){
+    res.sendFile(path.join(__dirname,"./script.js"),casheOptions)
+
+  }else{
+    res.status(403).send('Forbidden')
+  }
 })
+
 app.get("/images/:filename",(req,res)=>{
   const {filename} = req.params
-  res.sendFile(path.join(__dirname,`../public/images/${filename}`))
+  res.sendFile(path.join(__dirname,`../public/images/${path.basename(filename)}`))
 })
+
 app.get("/images/icons/:filename",(req,res)=>{
   const {filename} = req.params
   res.sendFile(path.join(__dirname,`../public/images/icons/${filename}`))
 })
+
 app.get("/fonts/:filename",(req,res)=>{
   const {filename} = req.params
   res.sendFile(path.join(__dirname,`../public/fonts/${filename}`))
 })
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- src/script.js
+++ src/script.js
@@ -2,4 +2,6 @@ const menu = document.getElementById('menu-btn')
 const menuItems = document.querySelector("#nav-menu")
 menu.addEventListener("click",()=>{
     menuItems.style.visibility = 'visible'
+    console.log('hello');
 })
+
